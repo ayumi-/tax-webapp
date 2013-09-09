50fb997ac1890aa5b3ad3c5908a55dd2cf86d0a1
ayumi <ayuim.hosozawa@gmail.com>
2013-09-09 20:48:44 +0900

    注文一覧をid順に変更

--- app/models/trading/TradingTransaction.java
+++ app/models/trading/TradingTransaction.java
@@ -65,7 +65,7 @@ public class TradingTransaction extends Transaction {
 			new Finder<Long, TradingTransaction>(Long.class, TradingTransaction.class);
 	
 	public static List<TradingTransaction> findByContract(Contract contract) {
-		return find.where().eq("contract_number", contract.contractNumber).order("id").findList();
+		return find.where().eq("contract_number", contract.contractNumber).order("transaction_number").findList();
 	}
 
 	public static TradingTransaction finndById(Long transactionNumber) {
